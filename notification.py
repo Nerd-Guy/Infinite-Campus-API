b5167c70ad1901225f1d603a7702a8eb737d81d3
Nerd-Guy <memeweaver.c@gmail.com>
2022-03-26 18:19:36 -0400

    Change to dataclass

--- notification.py
+++ notification.py
@@ -1,29 +1,22 @@
+from dataclasses import dataclass
 
+@dataclass(init=False, repr=False)
 class Notification:
-    """
-    Notifcation class for student notifications.
-    Each notification has a notificationID, userID,
-    creationTimestamp, notifcationTypeID, read,
-    notificationText, notificationTypeText,
-    displayedDate, finalText, and finalUrl.
-    """
-    # A dataclass should be used here but **unused needs to be here since we may get unexpected attributes passed. 
-    def __init__(self, notificationID: int, userID: int,
-        creationTimestamp: str, notificationTypeID: int,
-        read: bool, notificationText: str,
-        notificationTypeText: str,  displayedDate: str, 
-        finalText: str, finalUrl: str, **unused):
-        self.notificationID = notificationID
-        self.userID = userID
-        self.creationTimestamp = creationTimestamp
-        self.notificationTypeID = notificationTypeID
-        self.read = read
-        self.notificationText = notificationText
-        self.notificationTypeText = notificationTypeText
-        self.displayedDate = displayedDate
-        self.finalText = finalText
-        self.finalUrl = finalUrl
-        self.unused = unused
+    """Notifcation class for student notifications."""
+    notificationID: int 
+    userID: int
+    creationTimestamp: str
+    notificationTypeID: int
+    read: bool
+    notificationText: str
+    notificationTypeText: str  
+    displayedDate: str
+    finalText: str
+    finalUrl: str
+
+    def __init__(self, **kwargs) -> None:
+        for kwarg in kwargs.keys():
+            setattr(self, kwarg, kwargs[kwarg])
 
     def __repr__(self):
         return "{}: {}.".format(
